c4949967ec4adbd78a718a79fca28a006373fccb
Divyansh-98965 <divyansh98965@gmail.com>
2026-06-09 15:17:32 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s1 = s.strip().lower()
+
+        clean_text = "".join([char for char in s1 if char.isalnum()])
+        
+        for i in range(len(clean_text)):
+            n = len(clean_text)
+        
+            if clean_text[i] != clean_text[n - i - 1]:
+                return False
+        
+        return True
+        
