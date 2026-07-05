05b8a1415e663960ae36ee8fb7842f7df615d53d
Divyansh-98965 <divyansh98965@gmail.com>
2026-07-05 15:46:01 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.py
@@ -0,0 +1,36 @@
+
+class Solution:
+
+    def evalRPN(self, tokens: List[str]) -> int:
+        def int_change(string):
+            try:
+                k = int(string)
+                return k
+            except ValueError:
+                return False
+
+        stack = []
+        for i in tokens:
+            if (i=="*" or i=="+" or i=="-" or i=="/"):
+                right = stack.pop()
+                left = stack.pop()
+                operator = i
+                res = 0  
+                match operator:
+                    case "+":
+                        res = int((left) + (right))
+                    case "-":
+                        res = int((left) - (right)) 
+                    case "*":
+                        res = int((left) * (right)) 
+                    case "/":
+                        res = int((left)/(right))
+
+                stack.append(res)
+            else:
+                stack.append(int_change(i))
+
+        return (stack.pop())
+                    
+
+        
---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        #brute force solution
+        n = len(nums)
+        res = [1] * n
+        prefix_array = [1] * n 
+        suffix_array = [1] * n
+
+        for i in range(1,n):
+            prefix_array[i] = nums[i - 1] * prefix_array[i - 1]
+        
+    
+        # Loop backwards: element at i depends on what is to its right
+        for i in range(n - 2, -1, -1):
+            suffix_array[i] = suffix_array[i + 1] * nums[i + 1]
+        
+        for i in range(n):
+            res[i] = prefix_array[i] * suffix_array[i]
+        
+        return res
